b63ae60f048e0903a443bb4fc0203774f0fbe504
9uebangeT <139662232+9uebangeT@users.noreply.github.com>
2023-07-16 12:07:49 +0700

    Add files via upload

---
+++ 1.css
@@ -0,0 +1,127 @@
+  Silahkan login untuk masuk ke member area pasirsabuk ltd<br><br><style>
+
+
+/* -----------------------------------------------
+login page Style
+Name  : Misbahul Fahmi Fahrezi
+Youtube : Go For Edu
+Date    : 30 Oktober 2020
+----------------------------------------------- */
+
+ .inputtext {
+  width: 100%;
+  vertical-align: middle;
+  margin: 5px 10px 5px 0;
+  padding: 10px;
+  background-color: 75C2F6;
+  border: 2px solid #000;
+  border-radius: 20px;
+  box-sizing: border-box;
+
+}
+
+.tombol {
+  width: 100%;
+  vertical-align: middle;
+  margin: 5px 10px 5px 0;
+  padding: 10px;
+  background-color: FF6666;
+  border: 2px solid #000;
+  border-radius: 20px;
+  box-sizing: border-box;
+  text-transform: uppercase;
+}
+
+div {
+  font-family: arial;
+  font-weight: bold;
+  
+}
+
+#timer{ 
+color: FE0000;
+}
+
+ body {
+  overflow-y: hidden; /* Hide vertical scrollbar */
+  overflow-x: hidden; /* Hide horizontal scrollbar */
+}
+
+</style>
+
+<body>
+
+<center>
+
+<center>
+
+<div class="div1">
+
+<form name="myform" method="post">
+
+  <input type="text" name="uname" class="inputtext" autocomplete="off" placeholder="Pasirsabuk Username">
+  <br>
+  <input type="password" name="pass" class="inputtext" autocomplete="off" placeholder="Pasirsabuk Password">
+  <br>
+  <div id="timer"></div>
+  <br>
+  <input type="button" class="tombol" name="log" id="logbutton" value="Login" onclick="myfunction()">
+  <br><br>
+
+
+</form>
+
+
+</div>
+
+</center>
+
+</center>
+
+</body>
+
+<script type="text/javascript">
+  
+  function myfunction ()
+  {
+
+    var un = document.forms["myform"]["uname"].value;
+    var pw = document.forms["myform"]["pass"].value;
+    if (un=="zadam" && pw=="9uebangeT")
+  {
+    window.open('https://www.pasirsabuk.com/jenal', '_blank');
+  }
+
+/* ---------Copy code user dibawah ini---------- */
+
+
+
+
+else if (un=="admin" && pw=="9uebangeT")
+  {
+    window.open(' https://www.pasirsabuk.com/admin ', '_blank');
+}
+
+
+
+
+
+
+/* ---------Copy code user diatas ini-------- */
+
+
+    else
+  {
+    document.getElementById("timer").innerHTML = "Punten... password atawa username nage lepat ning... cobian deui geura cing taliti...atanapi lapor ka admin upami hilap atawa bade register janten kulawargi pasirsabuk ltd... nuhun..!!!";
+  }
+  }
+  
+</script>
+<body><right>
+<a href="https://www.pasirsabuk.com/register" target="_blank">
+<button class="button">Daftar</button></a>
+</body>
+<body><right>
+<a href="https://www.pasirsabuk.com/l-password" target="_blank">
+<button class="button">Lupa password</button></a>
+</body>
